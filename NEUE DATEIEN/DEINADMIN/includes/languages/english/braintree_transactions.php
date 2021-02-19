4d2ccd2be06c91663d313b05b9ac869d5a14bdb0
Jörg Schönberger <office@webchills.at>
2021-02-19 16:45:08 +0100

    Version 2.1.0

---
+++ NEUE DATEIEN/DEINADMIN/includes/languages/english/braintree_transactions.php
@@ -0,0 +1,27 @@
+<?php
+define('FILENAME_BRAINTREE_TRANSACTIONS', 'braintree_transactions');
+define('HEADING_ADMIN_TITLE', 'Braintree Transaktionen');
+define('TABLE_HEADING_ORDER_NUMBER', 'Shop Bestellnummer');
+define('TABLE_HEADING_BRAINTREE_ID', 'Braintree Bestellnummer');
+define('TABLE_HEADING_TRANSACTION_ID', 'ID');
+define('TABLE_HEADING_BRAINTREE_TRANSACTION', 'Transaktions ID');
+define('TABLE_HEADING_BRAINTREE_DATE','Datum');
+define('TABLE_HEADING_PAYMENT_TYPE', 'Zahlungsart');
+define('TABLE_HEADING_PAYMENT_STATUS', 'Status');
+define('TABLE_HEADING_PAYMENT_MESSAGE', 'Rückmeldung');
+define('TABLE_HEADING_PAYMENT_REFNUM', 'Referenznummer');
+define('TABLE_HEADING_ACTION', 'Aktion');
+define('MAX_DISPLAY_SEARCH_RESULTS_PAYPAL_IPN', 50);
+define('TEXT_INFO_BRAINTREE_RESPONSE_BEGIN', 'Braintree Zahlung ');
+define('TEXT_INFO_BRAINTREE_RESPONSE_END', ' für Bestellung');
+define('HEADING_PAYMENT_STATUS', 'Status');
+define('TEXT_BRAINTREE_SORT_ORDER_INFO', 'Anzeigesortierung');
+define('TEXT_SORT_BRAINTREE_ID_DESC', 'Braintree Sortierung (neu-alt)');
+define('TEXT_SORT_BRAINTREE_ID', 'Braintree Sortierung (alt-neu)');
+define('TEXT_SORT_ZEN_ORDER_ID_DESC', 'Shop Bestellnummer (neu-alt)');
+define('TEXT_SORT_ZEN_ORDER_ID', 'Shop Bestellnummer (alt-neu)');
+define('TEXT_BRAINTREE_BRAND_DESC', 'Kartentyp absteigend');
+define('TEXT_BRAINTREE_BRAND', 'Kartentyp aufsteigend');
+define('TEXT_BRAINTREE_STATE', 'Status');
+define('TEXT_BRAINTREE_PAYMENT_TYPE', 'Kreditkarte');
+define('TEXT_ALL_IPNS', 'Alle');
